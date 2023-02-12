be99dd5a19493555e36bcbfe4ab4472bd95cab5b
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-12 19:28:35 -0300

    Update home.php

--- home.php
+++ home.php
@@ -171,7 +171,7 @@
                     </button>
                 </div>
                 <div class="modal-body" id="botoes_auto_preenchidos">
-		    <strong> Suas respostas pessoais</strong>
+		    <strong> Suas respostas pessoais</strong><br> 
 			
                     <strong>Equipamentos Ópticos</strong> <br>                    
                     <button class="btn btn-outline-dark" id="fun_Fiberhome" onclick="fun_Fiberhome()">Fiberhome</button>
