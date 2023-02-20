1927794006911e277577b62cfd04d4d977149063
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-20 20:14:06 -0300

    Update conection.php

--- app/conection.php
+++ app/conection.php
@@ -84,16 +84,16 @@
         /*  Update user config */
 		public function update($db, $query){	   
 		    try{
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stmt = $db->prepare($query);
-                $stmt->execute();
+               		 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               		 $stmt = $db->prepare($query);
+                	 $stmt->execute();
                 
-                return $stmt->rowCount();
+                         return $stmt->rowCount();
                         
                
 			  	
 			}catch( PDOException $Exception){
-    			echo ( $Exception->getMessage()   ) ;
+    				echo ( $Exception->getMessage()   ) ;
 				echo ( (int)$Exception->getCode() );
 			}
 		    
@@ -101,7 +101,7 @@
 		}
 
         /*Read User data*/
-		public function read($db, $query){
+	public function read($db, $query){
 			try{
 			        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         			$stmt = $db->prepare($query);
@@ -117,15 +117,13 @@
         			  	
         			  	
 			}catch( PDOException $Exception){
-    			echo ( $Exception->getMessage()   ) ;
+    				echo ( $Exception->getMessage()   ) ;
 				echo ( (int)$Exception->getCode() );
 			}
-		}
-		
 	}
 
 	 /*Delete User data*/
-		public function delete($db, $query){
+	public function delete($db, $query){
 			try{
 			        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         			$stmt = $db->prepare($query);
@@ -140,11 +138,9 @@
         			  	
         			  	
 			}catch( PDOException $Exception){
-    			echo ( $Exception->getMessage()   ) ;
+    				echo ( $Exception->getMessage()   ) ;
 				echo ( (int)$Exception->getCode() );
 			}
-		}
-		
 	}
 
 ?>
