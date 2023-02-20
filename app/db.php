5ebc0612678dbc906b3644deffa367a38b727840
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-20 20:56:57 -0300

    Update db.php

--- app/db.php
+++ app/db.php
@@ -239,5 +239,23 @@
 	}
 
 
+	if(isset($_GET['action']) && $_GET['action'] == "deleteAnswer"){
+            session_start();
+	    if( isset($_SESSION['user']) && !empty($_SESSION['user']) ){ 
+	        if( isset($_SESSION['password']) && !empty($_SESSION['password']) ){
+		    $id_user = $_SESSION['idUser'];
+		    $id_answer = $_POST['id'];
+	            $query = "DELETE FROM `users_answers` WHERE `id` = $id_answer;"; 
+	            $pdo = new Connect();
+	            $db = $pdo->connectOnDb();
+		    $answer = $pdo->$pdo->delete($db, $query);
+                   echo ( $answer );	
+			
+	        }
+	    }
+	    
+	}
+
+
 
 ?>
