dff03027e91df5ef803ccfd697d063dfcb3cbb61
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-20 23:44:38 -0300

    Update config.php

--- config.php
+++ config.php
@@ -157,7 +157,17 @@
 <body style="background-color: rgb(53, 54, 63);" onload="getSavedConfig()" >
     <h1> <strong>Configurações da Plataforma</strong> </h1>
     
-     
+  <div id="Salvar">    
+        
+        <input type="submit" class="btn btn-warning"  value="Salvar" onclick="salvar()">  <br>  <br>
+        
+        <a href="./home.php" class="btn btn-success"> 
+        Home 
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/></svg>
+        </a>
+
+    </div>   
 
 <form method="post" action="./app.php?action=setRestBreak"><!--Acrescentei essa linha-->
     <div>
@@ -215,17 +225,7 @@
     </div>
 
     
-    <div id="Salvar">    
-        
-        <input type="submit" class="btn btn-warning"  value="Salvar" onclick="salvar()">  <br>  <br>
-        
-        <a href="./home.php" class="btn btn-success"> 
-        Home 
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/></svg>
-        </a>
-
-    </div>
+    
 
 	
 
