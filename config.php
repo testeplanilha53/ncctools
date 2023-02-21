2863b0e754b51ef4de73324afc4800e9f14270b8
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-20 22:29:26 -0300

    Update config.php

--- config.php
+++ config.php
@@ -101,7 +101,8 @@
             http.onreadystatechange = function() {  //Call a function when the state changes.
                 if(http.readyState == 4 && http.status == 200) {
                     let response = this.responseText;
-                    console.log(response)
+                    //console.log(response)
+		    document.location.reload(true);	
                 }
             }
             http.send(params);
