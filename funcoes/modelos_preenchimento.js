a965f7790f2986f8ae4ad9e33556cff5c576f5d4
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-24 23:03:05 -0300

    Update modelos_preenchimento.js

--- funcoes/modelos_preenchimento.js
+++ funcoes/modelos_preenchimento.js
@@ -648,7 +648,7 @@ function createAnswers(category, anwers){
 					//btn.classList.add("btn"); 
 					//btn.classList.add("btn-outline-dark");
 					 var label = document.createElement("label");
-					 label.innerHTML = "<button class='btn btn-outline-dark' id='" + el1['id'] + "' onclick='copy_answer(" + el1['id'] + ','+ el1['answer'] + ")>" + el1['title'] + "</button>"
+					 label.innerHTML = "<button class='btn btn-outline-dark' id='" + el1['id'] + "' onclick='copy_answer(" + el1['id'] + ", " + el1['answer'] + ")'>" + el1['title'] + "</button>"
 					 divConteiner.appendChild(label);
 				 }  
 			   });
