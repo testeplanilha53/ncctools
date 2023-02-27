672e19f9d00b7aae921d7b4df042f244c899a314
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-26 22:16:42 -0300

    Update email.js

--- funcoes/email.js
+++ funcoes/email.js
@@ -1,4 +1,3 @@
-/* Original Dione
 document.getElementsByClassName("lastPass")[0].addEventListener('keyup', (e) => {
     let firstPass =   document.getElementsByClassName("firstPass")[0].value.trim();
     let lastPass  =   document.getElementsByClassName("lastPass")[0].value.trim();
@@ -14,49 +13,7 @@ function showAlert(bool){
     }else if(bool == true && !alert.classList.contains("hiddenAlert") ){
         alert.classList.add("hiddenAlert");
     }
-}*/
-
-
-
-//EDIÇÃO LUCAS
-
-document.getElementsByClassName("lastPass")[0].addEventListener('keyup', (e) => {
-    let firstPass =   document.getElementsByClassName("firstPass")[0].value.trim();
-    let lastPass  =   document.getElementsByClassName("lastPass")[0].value.trim();    
-    let size = lastPass.length - 1;
-//    showAlert(lastPass[size] == firstPass[size]);    
-    if (firstPass!="" && lastPass!=""){
-       showAlert(lastPass[size] == firstPass[size]);    
-    }
-});
-
-
-document.getElementsByClassName("firstPass")[0].addEventListener('keyup', (e) => {
-    let firstPass =   document.getElementsByClassName("firstPass")[0].value.trim();
-    let lastPass  =   document.getElementsByClassName("lastPass")[0].value.trim();
-    let size = lastPass.length - 1;
-    //    showAlert(lastPass[size] == firstPass[size]);    
-    if (firstPass!="" && lastPass!=""){
-       showAlert(lastPass[size] == firstPass[size]);    
-    }
-});
-
-
-function showAlert(bool){
-    let alert = document.getElementsByClassName("showAlert")[0];
-    let button = document.getElementsByClassName("showAlert")[1];
-    console.log(button)
-
-    if(bool == false && alert.classList.contains("hiddenAlert") ){
-        alert.classList.remove("hiddenAlert");
-        button.classList.add("hiddenAlert");
-    }else if(bool == true && !alert.classList.contains("hiddenAlert") ){
-        alert.classList.add("hiddenAlert");
-        button.classList.remove("hiddenAlert");
-    }
-
-    if(bool == true){
-        button.classList.remove("hiddenAlert");   
-    }
-         
 }
+
+
+
