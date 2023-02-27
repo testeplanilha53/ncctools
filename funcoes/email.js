98b221bb0d3dc01ab91c427844915e8cf5b6e16c
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-02-26 22:29:09 -0300

    Update email.js

--- funcoes/email.js
+++ funcoes/email.js
@@ -6,6 +6,18 @@ document.getElementsByClassName("lastPass")[0].addEventListener('keyup', (e) => 
 });
 
 
+document.getElementsByClassName("firstPass")[0].addEventListener('keyup', (e) => {
+    let firstPass =   document.getElementsByClassName("firstPass")[0].value.trim();
+    let lastPass  =   document.getElementsByClassName("lastPass")[0].value.trim();
+    let size = lastPass.length - 1;
+    if(lastPass.length > 0){
+        showAlert(lastPass[size] == firstPass[size]);   
+    }
+    
+});
+
+
+
 function showAlert(bool){
     let alert = document.getElementsByClassName("showAlert")[0];
     if(bool == false && alert.classList.contains("hiddenAlert") ){
