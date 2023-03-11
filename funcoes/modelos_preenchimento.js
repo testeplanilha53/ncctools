cf3059677839f98b3911329bc04b06ad01f6d60b
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-03-10 21:42:51 -0300

    Update modelos_preenchimento.js

--- funcoes/modelos_preenchimento.js
+++ funcoes/modelos_preenchimento.js
@@ -17,10 +17,9 @@ function change_color(meu_id){
 }
 
 
-
+var divConteiner = document.querySelectorAll(".preenchidos_conteiner")[0]; 	
+var db_elements = 	divConteiner.querySelectorAll(`[id^="fun_"]`)
 function db_change_color(meu_id){
-   let divConteiner = document.querySelectorAll(".preenchidos_conteiner")[0]; 	
-   let db_elements = 	divConteiner.querySelectorAll(`[id^="fun_"]`)
     db_elements.forEach(element => {
         if (element.id!="meu_id"){
 		console.log(meu_id)
