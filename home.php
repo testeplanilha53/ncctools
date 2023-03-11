777b098ef3e148f26b5f9710ac83da081fd7c9fb
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-03-11 16:38:35 -0300

    Update home.php

--- home.php
+++ home.php
@@ -296,7 +296,8 @@
 			<p id="tags_html">
 			   <button onclick="add_tag('<br>')">&lt;br&gt;</button>	
 			   <button onclick="add_tag('<hr>')">&lt;hr&gt;</button>
-			   <button onclick="add_tag('<ul>')">&lt;ul&gt; + &ltli&gt;</button>
+			   <button onclick="add_tag('<ul><li>ALTERE AQUI ENTRE AS TAGS</li></uL>')">&lt;ul&gt; + &ltli&gt;</button>
+			  <button onclick="add_tag('<ul>')">&lt;ul&gt;</button>	
 			   <button onclick="add_tag('</ul>')">&lt;/ul&gt;</button>
 			   <button onclick="add_tag('<li>')">&lt;li&gt;</button>
 			   <button onclick="add_tag('</li>')">&lt;/li&gt;</button>
