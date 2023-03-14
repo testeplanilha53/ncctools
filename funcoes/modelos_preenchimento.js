dc8c822dd8a9cd1122d17d9dcf3c40d9156616e9
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-03-14 02:45:11 -0300

    Update modelos_preenchimento.js

--- funcoes/modelos_preenchimento.js
+++ funcoes/modelos_preenchimento.js
@@ -710,7 +710,7 @@ function enableTags(){
    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-		console.log( this.responseText );
+		console.log( JSON.parse( this.responseText) );
 		
           }
      };
