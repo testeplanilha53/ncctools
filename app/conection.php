cddc837b61413489b03bcb7fc6ebefd9b4297ec8
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-03-24 19:55:16 -0300

    Update conection.php

--- app/conection.php
+++ app/conection.php
@@ -40,6 +40,7 @@
                         $_SESSION['user'] =  $username;
                         $_SESSION['password'] = $password;
 
+			$this->create($db, "all_logs", "DEFAULT, `login`,'".$username."', '".date("d-m-Y")."'"   );    
                         $file = fopen($_SERVER["SERVER_ROOT"]."/all_logs/log-".date("d-m-Y").".txt", "a+");
                         fwrite($file, "Login: ".$username." on ".date("d-m-Y H:i:s")."\r\n" );
                         fclose($file);
