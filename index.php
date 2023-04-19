80fe636fbeb8b3f5248c9a35300b38fe4f07e01e
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-04-18 22:36:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -272,7 +272,7 @@
 
             <!--12/01 Acrescentado essa linha-->
             <span style="color: white; font-size: 14px; display: block; margin: 5px; auto; text-align: center;" id="resetMail" onclick="window.modalDialog.showModal();">
-            Esqueceu sua senha?</span> 
+            Esqueceu sua senha? XXX</span> 
             
 
             <button>
