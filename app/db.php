696ece281fc881c2a943e0a88cc323ef6ef140ee
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-01 22:16:02 -0300

    Update db.php

--- app/db.php
+++ app/db.php
@@ -361,8 +361,13 @@
 	            $pdo = new Connect();
 	            $db = $pdo->connectOnDb();
 		    $answer = $pdo->read($db, $query );
-		    $answer[0]['adm_protocol'] = base64_decode( $answer[0]['adm_protocol'] );
-                    $answer[0]['description'] = base64_decode( $answer[0]['description'] );
+		
+			if( $answer != false ){
+				$answer[0]['number_protocol'] = $number_protocol;
+				$answer[0]['adm_protocol'] = base64_decode( $answer[0]['adm_protocol'] );
+                   		$answer[0]['description'] = base64_decode( $answer[0]['description'] );
+			}
+		    
                 	
 		    header('Content-Type: application/json');	
 		    $array = json_encode($answer, JSON_UNESCAPED_UNICODE );
