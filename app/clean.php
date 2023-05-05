4251e96431e8dfb127ffd89d6c863db588361a69
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-04 22:00:40 -0300

    Update clean.php

--- app/clean.php
+++ app/clean.php
@@ -7,5 +7,14 @@
 		$pdo = new Connect();
 		$db = $pdo->connectOnDb();
 		$answer = $pdo->delete($db, $query);
+		
+		$today = date("Y-m-d);
+		$other = date("Y-m-d  2023-05-04");
+		
+		if($today > $other ){
+			echo "É maior";
+		
+		}
+
 
 	?>
