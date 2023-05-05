292497f9b3607d1b8dcd68980c9416cb59b9f328
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-04 22:20:40 -0300

    Update clean.php

--- app/clean.php
+++ app/clean.php
@@ -2,19 +2,10 @@
 		
 		require_once('conection.php');
 
-		$query = "DELETE FROM `saved_pending`  WHERE  `date` >= CURRENT_DATE) "; 
+		$query = "DELETE FROM `saved_pending`  WHERE  `date` <= CURRENT_DATE) "; 
 
 		$pdo = new Connect();
 		$db = $pdo->connectOnDb();
 		$answer = $pdo->delete($db, $query);
-		
-		$today = date("Y-m-d);
-		$other = date("Y-m-d  2023-05-04");
-		
-		if($today > $other ){
-			echo "É maior";
-		
-		}
-
 
 	?>
