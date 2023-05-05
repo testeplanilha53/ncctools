f9a958ef8cf823c5f78e94f60726c3c8f9dcba33
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-04 22:52:28 -0300

    Update clean.php

--- app/clean.php
+++ app/clean.php
@@ -2,10 +2,13 @@
 		
 		require_once('conection.php');
 
-		$query = "DELETE FROM `saved_pending`  WHERE  `date` <= CURRENT_DATE) "; 
+		$query = "DELETE FROM `saved_pending`  WHERE  `date` <= CURRENT_DATE "; 
 
 		$pdo = new Connect();
 		$db = $pdo->connectOnDb();
 		$answer = $pdo->delete($db, $query);
+
+
+		//echo date('Y-m-d', strtotime('-1 days', strtotime('2017-11-07')));
 
 	?>
