7a6347c321e3f21401793e1edcba1aa93b96d3be
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-07 17:50:06 -0300

    att

--- home.php
+++ home.php
@@ -57,7 +57,7 @@
                     </span>
                 </div>
             </div>
-            <nav class="navbar nav_cor">
+            <nav class="navbar navbar-dark bg-dark">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
