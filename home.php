1d7bd93ec5ba32b7ea6e71d54d5d4e406c67c22c
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-07 17:43:37 -0300

    att

--- home.php
+++ home.php
@@ -51,7 +51,7 @@
             <div class="collapse" id="navbarToggleExternalContent">
                 <div class="bg-dark p-4">
                     <h4 class="text-white">Desenvolvedores:</h4>
-                    <span class="text-muted">                    
+                    <span class="text-white">                    
                         dione.mourao@ncc.faster.com.br <br>
                         lucas.aguiar@ncc.faster.com.br
                     </span>
