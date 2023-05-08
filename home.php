f9312616dfba63e87194863d09f82028a99766e7
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-08 20:17:48 -0300

    Adicionado a header

--- home.php
+++ home.php
@@ -42,7 +42,7 @@
 <body style="background-color: #35363f;" onmousemove="mudar_titulo()" id="corpo_tela_principal">
     <!-- rgb(40,42,54) -->
 
-    <!-- Image and text -->
+    <!-- Image and text 
     <nav class="navbar nav_cor">
         <span class="navbar-brand">        
             NCCTools        
@@ -63,8 +63,56 @@
                 </button>
             </nav>
         </div>
-    </nav>
+    </nav> -->
+	
+	
+	<!--HEADER-->
+<header class="conteiner-fluid _header">
+  <nav class="navbar text-white m-auto p-2" style="max-width: 1920px;">
+    <!--MENU-NAV-->
+    <label class="navbar-item text-white _header_label">
+      <a href="#" class="navbar-brand text-white p-0 m-0 ">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 18 18">
+        <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+        <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+      </svg>
+      Tools  
+    </a>
+    </label>
+    
+    <label class="navbar-item d-flex justify-content-center _header_label _clock d-flex d-flex justify-content-around px-3">
+      <span class="col">  1º </span>
+      <svg class="col" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-stopwatch" viewBox="0 0 20 20">
+        <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+        <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+      </svg>
+      <span class="col">00:23:30</span>
+      
+      
+    </label>
+    
+    <label class="navbar-item d-flex justify-content-end _header_label">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 18 18" onclick="menu()">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+    </label>
+  </nav>
+  <!--END MENU-NAV-->
+</header>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
+	<!---
     <header>
         
         <button type="button" id="fechar" class="btn btn-danger" onclick="javascript:window.close()">Fechar</button>        
@@ -149,6 +197,9 @@
             
         </div>
     </header>
+
+	 -->
+
 
     <!-- Modal Notificação de novidades -->
     <div class="modal fade" id="notificacaoNovidades" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
