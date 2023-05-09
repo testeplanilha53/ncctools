f1b36a653b9c8e4ceffdf21f0c3fb5648cfe2826
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-09 18:28:44 -0300

    Atualizado o css da home

--- home.php
+++ home.php
@@ -36,6 +36,9 @@
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
 
 	<style>
+		body{
+			overflow-x: hidden !important;
+		}
 		._header{
 border-bottom: solid 2px #151854;
 background-color: #152238;
