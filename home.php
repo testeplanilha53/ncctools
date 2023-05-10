df49456429d22f03293dab765629b424a3a5498e
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-09 23:48:18 -0300

    att

--- home.php
+++ home.php
@@ -133,31 +133,6 @@ height: 100vh;
 	
 
 <body style="background-color: #35363f;" onmousemove="mudar_titulo()" id="corpo_tela_principal">
-    <!-- rgb(40,42,54) -->
-
-    <!-- Image and text 
-    <nav class="navbar nav_cor">
-        <span class="navbar-brand">        
-            NCCTools        
-        </span>
-        <div class="pos-f-t">
-            <div class="collapse" id="navbarToggleExternalContent">
-                <div class="rounded bg-dark p-4">
-                    <h4 class="text-white">Desenvolvedores:</h4>
-                    <span class="text-muted">                    
-                        dione.mourao@ncc.faster.com.br <br>
-                        lucas.aguiar@ncc.faster.com.br
-                    </span>
-                </div>
-            </div>
-            <nav class="navbar navbar-dark nav_cor">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </nav>
-        </div>
-    </nav> -->
-	
 	
 	<!--HEADER-->
 <header class="conteiner-fluid _header">
@@ -169,7 +144,7 @@ height: 100vh;
         <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
         <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
       </svg>
-      Tools  
+      NCCTools
     </a>
     </label>
 
@@ -188,11 +163,12 @@ height: 100vh;
         <a href="./home.php" target="_blank" id="novo">
             <button type="button" class="btn btn-success">Nova Aba</button>
         </a>
+
+        <br><span id="notificacao"> </span>
     </span>
 
     <span>
-        <br>             
-        <span id="notificacao"> </span>
+        <br>                     
         <span id="timer"></span>                   
     </span>
 	  
