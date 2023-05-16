008797d93080b56edfb484046037dfdeb8b3a37f
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-15 22:49:48 -0300

    att

--- home.php
+++ home.php
@@ -139,13 +139,13 @@ height: 100vh;
   <nav class="navbar text-white m-auto p-2" style="max-width: 1920px; background: rgb(53, 54, 63);">
     <!--MENU-NAV-->
     <label class="navbar-item text-white _header_label">
-      <a href="#" class="navbar-brand text-white p-0 m-0 ">
-      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 18 18">
-        <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
-        <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
-      </svg>
+      <a href="#" class="navbar-brand text-white p-0 m-0 " data-toggle="tooltip" data-placement="bottom" title="Para atendimentos Faster...">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 18 18">
+            <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+            <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+        </svg>
       NCCTools
-    </a>
+      </a>
     </label>
 
     <span id="botoes_mais_usuais">
@@ -404,7 +404,7 @@ height: 100vh;
                     R$ <input type="text" class="campo_valor" id="campo_valor2" placeholder="Valor: R$ X,XX "> - Data: <input type="text" class="campo_valor" id="data_valor2" placeholder="Venc.: DD/MM/AAAA"><br>
                     R$ <input type="text" class="campo_valor" id="campo_valor3" placeholder="Valor: R$ X,XX "> - Data: <input type="text" class="campo_valor" id="data_valor3" placeholder="Venc.: DD/MM/AAAA"><br>
                     R$ <input type="text" class="campo_valor" id="campo_valor4" placeholder="Valor: R$ X,XX "> - Data: <input type="text" class="campo_valor" id="data_valor4" placeholder="Venc.: DD/MM/AAAA"><br>
-                    R$ <input type="text" class="campo_valor" id="campo_valor5" placeholder="Valor: R$ X,XX "> - Data: <input type="text" id="data_valor5" placeholder="Venc.: DD/MM/AAAA"><br>                                      
+                    R$ <input type="text" class="campo_valor" id="campo_valor5" placeholder="Valor: R$ X,XX "> - Data: <input type="text" class="campo_valor" id="data_valor5" placeholder="Venc.: DD/MM/AAAA"><br>                                      
                 </div>
                 <div class="modal-footer">                                    
                     <button type="button" class="btn btn-secondary"  onclick="apagar_modelo_financeiro()">Apagar</button>                    
