ec89602c932e6a10510445e58766f7309b52e29e
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-15 22:20:16 -0300

    tt

--- funcoes/padrao_noc.js
+++ funcoes/padrao_noc.js
@@ -5,6 +5,7 @@ function copiar_padrao(){
     var mensagem = "Olá, pode verificar?\n"
     
     mensagem += `SN: ${elements[0].value}\nUsuário: ${elements[1].value}\nCidade: ${elements[2].value}\nAutenticação: ${elements[3].value}\nPlano: ${elements[4].value}\nEndereço: ${elements[5].value}\nMotivo da verificação: ${elements[6].value}\nNome e nº de telefone do cliente: ${elements[7].value}`
+    mensagem += `\nApresenta risco de acidente: ${elements[8].value}`
     console.log(mensagem)
 
     // Passando as informações para a área de transferência
