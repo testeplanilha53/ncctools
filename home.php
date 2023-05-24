bc5faf95c95e88edd628275a2f401e7b09f1ed6f
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-24 20:15:26 -0300

    att

--- home.php
+++ home.php
@@ -170,7 +170,7 @@ height: 100vh;
         <!--RELÓGIOS-->
         <br>
         <span id="relogios">
-            <span id="button_clock1">
+            <span id="button_clock1" class="clock">
                 <button  class="btn btn-outline-light" data-toggle="tooltip" data-placement="top" title="Cronometro para a 1ª pausa">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
                         <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
@@ -182,7 +182,7 @@ height: 100vh;
                 </button>
             </span>
             
-            <span id="button_clock2">
+            <span id="button_clock2" class="clock">
                 <button class="btn btn-outline-light" data-toggle="tooltip" data-placement="top" title="Cronometro para o intervalo">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
                         <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
@@ -194,7 +194,7 @@ height: 100vh;
                 </button>
             </span>
             
-            <span id="button_clock3">
+            <span id="button_clock3" class="clock">
                 <button class="btn btn-outline-light" data-toggle="tooltip" data-placement="top" title="Cronometro para a 2ª pausa">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
                         <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
