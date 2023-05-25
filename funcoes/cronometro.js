f7360b50d692c981dd4126c3f0b8e261fa178488
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-05-24 22:05:39 -0300

    teste

--- funcoes/cronometro.js
+++ funcoes/cronometro.js
@@ -41,7 +41,35 @@ var tempo3 = converter_segundos_string(pausa_2) - converter_segundos(agora)
   cronometroDecrescente(tempo1,campo_timer1,button_clock1); // Inicia o cronômetro decrescente com 3665 segundos (1 hora, 1 minuto e 5 segundos)
   cronometroDecrescente(tempo2,campo_timer2,button_clock2); // Inicia o cronômetro decrescente com 3665 segundos (1 hora, 1 minuto e 5 segundos)
   cronometroDecrescente(tempo3,campo_timer3,button_clock3); // Inicia o cronômetro decrescente com 3665 segundos (1 hora, 1 minuto e 5 segundos)
+
+
+
+  function cronometroDecrescente(segundos,campo_timer,button_clock) {    
+    var intervalo = setInterval(() => {
+              
+      var horas = Math.floor(segundos / 3600);
+      var minutos = Math.floor((segundos % 3600) / 60);
+      var segundosRestantes = segundos % 60;
   
+      var formatado = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundosRestantes.toString().padStart(2, '0')}`;
+      console.log(formatado);
+                  
+      if (minutos <= 10) {
+        button_clock.innerHTML = `<button  class="btn btn-outline-light clock_new" data-toggle="tooltip" data-placement="top" title="Cronometro para a 1ª pausa"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16"> <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/> <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/> </svg> <span id="cronometro_pausas1"> ${formatado} </span> </button>`
+        //campo_timer.innerHTML = `${formatado}`        
+      }      
+      
+      if (segundos <= 0) {
+        clearInterval(intervalo);
+        button_clock.innerHTML=``                
+      }
+  
+      segundos--;
+    }, 1000);
+  }
+
+
+  /*
 function cronometroDecrescente(segundos,campo_timer,button_clock) {    
     var intervalo = setInterval(() => {
               
@@ -64,6 +92,6 @@ function cronometroDecrescente(segundos,campo_timer,button_clock) {
       segundos--;
     }, 1000);
   }
-  
+  */
 
   
