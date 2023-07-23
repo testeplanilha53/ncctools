6844c3424a2f6252f7e7fd52a3acc89308ee7181
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-07-23 09:58:47 -0300

    att

--- home.php
+++ home.php
@@ -177,6 +177,11 @@
             <span class="mx-2">Treinamento</span>
             </a>
         </li>
+        <li>    
+            <span><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M339.3 367.1c27.3-3.9 51.9-19.4 67.2-42.9L568.2 74.1c12.6-19.5 9.4-45.3-7.6-61.2S517.7-4.4 499.1 9.6L262.4 187.2c-24 18-38.2 46.1-38.4 76.1L339.3 367.1zm-19.6 25.4l-116-104.4C143.9 290.3 96 339.6 96 400c0 3.9 .2 7.8 .6 11.6C98.4 429.1 86.4 448 68.8 448H64c-17.7 0-32 14.3-32 32s14.3 32 32 32H208c61.9 0 112-50.1 112-112c0-2.5-.1-5-.2-7.5z"/></svg>
+                &nbsp Mudar plano de fundo
+            </span>
+        </li>
 
         <li>    
             <a class="nav-link link-dark d-flex align-items-center sidebar_item text-white" id="btn_sair" href="./logout.php" class="btn "   data-toggle="tooltip" data-placement="top" title="Logout">              
