dbdbcaa4d4d1c5cdecc6564bfe8d314ab96aa1ae
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-07-26 14:35:54 -0300

    Update home.php

--- home.php
+++ home.php
@@ -34,7 +34,40 @@
 
     <!--FONTAWOSONE-->
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
-	
+    <style>
+	/* Estilos do botão */
+	.botao{
+		background: #5E5DF0;
+		border-radius: 999px;
+		box-shadow: #5E5DF0 0 10px 20px -10px;
+		box-sizing: border-box;
+		color: #FFFFFF;
+		cursor: pointer;
+		font-family: Inter,Helvetica,"Apple Color Emoji","Segoe UI Emoji",NotoColorEmoji,"Noto Color Emoji","Segoe UI Symbol","Android Emoji",EmojiSymbols,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue","Noto Sans",sans-serif;
+		font-size: 16px;
+		font-weight: 700;
+		line-height: 24px;
+		opacity: 1;
+		outline: 0 solid transparent;
+		padding: 8px 18px;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+		width: fit-content;
+		word-break: break-word;
+		border: 0;
+		text-decoration: none;
+	}
+
+
+
+	/* Estilos para o link */
+	.link{
+	  text-decoration: none;
+	  color: white;
+	  font-weight: bold;
+	}
+  </style>
 
 </head>
 	
@@ -199,7 +232,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
                     <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6zM13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6z"/>
                 </svg> Desenvolvedores <br>
-                &nbsp- Dione Mourão <br>
+                &nbsp- <a class="link" href="https://silvadevbr.rf.gd/">Dione Mourão</a><a class="botao" href="https://silvadevbr.rf.gd/">Novo</a><br>
                 &nbsp- Lucas Aguiar <br>
             </a>
         </li>
