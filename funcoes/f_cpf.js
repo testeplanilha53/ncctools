4beb71b5a1752430f05787e3980efa42a7dd3e4e
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-10-10 23:32:31 -0300

    att

--- funcoes/f_cpf.js
+++ funcoes/f_cpf.js
@@ -3,3 +3,12 @@ var areaCPF = window.document.getElementById("areaCPF");
 function libera_campo_cpf(){
     areaCPF.innerHTML = `<button type="button" id="btn_cpf" class="btn btn-secondary btn_atalho2" data-toggle="tooltip" data-placement="top" title="Após copiar o CPF, clique aqui." onclick="cpf()" ondblclick="x()" onchange="cpf_corrigir()">CPF</button>`
 }
+
+
+
+function shakeSVG(svgElement) {
+    svgElement.classList.add('shake');
+    setTimeout(() => {
+        svgElement.classList.remove('shake');
+    }, 500);
+}
