62c508e392a90d8ea951e4febcc707466d545fac
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-10-12 20:37:43 -0300

    att

--- funcoes/f_botoes.js
+++ funcoes/f_botoes.js
@@ -500,6 +500,60 @@ function cpfADM(){
 }
 
 
+function validarCNPJ(cnpj) {
+ 
+    cnpj = cnpj.replace(/[^\d]+/g,'');
+ 
+    if(cnpj == '') return false;
+     
+    if (cnpj.length != 14)
+        return false;
+ 
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999")
+        return false;
+         
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0,tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+         
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+          return false;
+           
+    return true;
+    
+}
+
 
 // Verificar CPF
 function validaCPF(strCPF) {
@@ -523,12 +577,12 @@ function validaCPF(strCPF) {
         return true;
 }
   
-//   Exemplo de uso:
-  const cpf1 = '123.456.789-09';
-  const cpf2 = '111.222.333-44';
+// //   Exemplo de uso:
+//   const cpf1 = '123.456.789-09';
+//   const cpf2 = '111.222.333-44';
   
-  console.log(validaCPF(cpf1)); // Deve imprimir true
-  console.log(validaCPF(cpf2)); // Deve imprimir false
+//   console.log(validaCPF(cpf1)); // Deve imprimir true
+//   console.log(validaCPF(cpf2)); // Deve imprimir false
   
 
 
@@ -539,7 +593,9 @@ function x(){
     var atalho = localStorage.getItem('txt_cpf');
     
     var cpfValido = validaCPF(atalho)
-    if (cpfValido){
+    var cnpjValido = validarCNPJ(atalho)
+
+    if (cpfValido||cnpjValido){
         // Abrindo no adm
         cpfADM()
 
