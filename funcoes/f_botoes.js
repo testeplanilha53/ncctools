e2fb1fb3f01ca08a4cdc5770824e524561cb46f0
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-10-31 23:56:11 -0300

    att

--- funcoes/f_botoes.js
+++ funcoes/f_botoes.js
@@ -22,13 +22,6 @@ var b_tranferir = window.document.getElementById("Transferir")
 var b_apagar = window.document.getElementById("Apagar")
 
 
-descricao.addEventListener("onchange", function () {
-
-    descricao.innerHTML = descricao.value;
-
-  });
-
-
 
 /// Busca pendência se tiver salva no banco 
 function salvar_pendencia(){
