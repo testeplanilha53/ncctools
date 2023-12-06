2a6143777bdb88ea17ac0431e3c0ccc085a75515
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2023-12-05 22:20:38 -0300

    att

--- index.php
+++ index.php
@@ -15,6 +15,13 @@
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
     <title>Plataforma de atendimento</title>
 
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
     <style>
         
         #resetMail:hover{
@@ -309,6 +316,9 @@
       </div>
     </div>
     -->
+
+    <div class="alert alert-primary" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> <strong>Lembre-se dos idealizadores no NCCTools:  <br> Monitor (Lucas Aguiar), Operador (Dione Mourão) <br> Prazo para votação até 08/12/2023.</strong> </div>
+
 
 
     <div class="login-box">
