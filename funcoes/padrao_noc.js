aff0b0ce5c692d9e69662a4ca050b4bd36343e1e
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-02-05 21:49:51 -0300

    att

--- funcoes/padrao_noc.js
+++ funcoes/padrao_noc.js
@@ -4,9 +4,17 @@ function copiar_padrao(){
     var elements = document.querySelectorAll(`[class^="campo_preenchimento_ncc"]`)
     var mensagem = "Olá, pode verificar?\n"
     
-    mensagem += `SN: ${elements[0].value}\nUsuário: ${elements[1].value}\nCidade: ${elements[2].value}\nAutenticação: ${elements[3].value}\nPlano: ${elements[4].value}\nEndereço: ${elements[5].value}\nMotivo da verificação: ${elements[6].value}\nNome e nº de telefone do cliente: ${elements[7].value}`
-    if (elements[8].value!=""){
-        mensagem += `\nApresenta risco de acidente: ${elements[8].value}`
+    mensagem += `SN: ${elements[0].value}\n
+                 Usuário: ${elements[1].value}\n
+                 Cidade: ${elements[2].value}\n
+                 Autenticação: ${elements[3].value}\n
+                 Plano: ${elements[4].value}\n
+                 Endereço: ${elements[5].value}\n
+                 Motivo da verificação: ${elements[6].value}\n
+                 Nome: ${elements[7].value}\n
+                 nº de telefone do cliente: ${elements[8].value}\n`
+    if (elements[9].value!=""){
+        mensagem += `\nApresenta risco de acidente: ${elements[9].value}`
     }
     
     console.log(mensagem)
