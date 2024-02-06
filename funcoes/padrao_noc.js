5e5f8bf01e1e9779bfa9e98968a011ff4cb7ed97
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-02-05 21:57:17 -0300

    att

--- funcoes/padrao_noc.js
+++ funcoes/padrao_noc.js
@@ -4,6 +4,8 @@ function copiar_padrao(){
     var elements = document.querySelectorAll(`[class^="campo_preenchimento_ncc"]`)
     var mensagem = "Olá, pode verificar?\n"
     
+    //mensagem += `SN: ${elements[0].value}\n Usuário: ${elements[1].value}\n Cidade: ${elements[2].value}\n Autenticação: ${elements[3].value}\n Plano: ${elements[4].value}\n Endereço: ${elements[5].value}\n Motivo da verificação: ${elements[6].value}\n Nome: ${elements[7].value}\n nº de telefone do cliente: ${elements[8].value}\n`
+    /*
     mensagem += `SN: ${elements[0].value}\n
                  Usuário: ${elements[1].value}\n
                  Cidade: ${elements[2].value}\n
@@ -13,6 +15,16 @@ function copiar_padrao(){
                  Motivo da verificação: ${elements[6].value}\n
                  Nome: ${elements[7].value}\n
                  nº de telefone do cliente: ${elements[8].value}\n`
+    */
+
+
+    mensagem += `SN: ${elements[0].value}\n                 
+                 Cidade: ${elements[2].value}\n
+                 Autenticação: ${elements[3].value}\n
+                 Plano: ${elements[4].value}\n
+                 Endereço: ${elements[5].value}\n                 
+                 ${elements[7].value}(${elements[1].value}) nº ${elements[8].value}. ${elements[6].value}\n`
+
     if (elements[9].value!=""){
         mensagem += `\nApresenta risco de acidente: ${elements[9].value}`
     }
