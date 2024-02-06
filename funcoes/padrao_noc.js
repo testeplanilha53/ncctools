8b6d877b182a3a0938e68a9bcdfb6ccdd25dea17
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-02-05 22:00:00 -0300

    att

--- funcoes/padrao_noc.js
+++ funcoes/padrao_noc.js
@@ -17,14 +17,15 @@ function copiar_padrao(){
                  nº de telefone do cliente: ${elements[8].value}\n`
     */
 
-
+    mensagem += `SN: ${elements[0].value}\n Cidade: ${elements[2].value}\n Autenticação: ${elements[3].value}\n Plano: ${elements[4].value}\n Endereço: ${elements[5].value}\n ${elements[7].value}(${elements[1].value}) nº ${elements[8].value}. ${elements[6].value}\n`
+   /*
     mensagem += `SN: ${elements[0].value}\n                 
                  Cidade: ${elements[2].value}\n
                  Autenticação: ${elements[3].value}\n
                  Plano: ${elements[4].value}\n
                  Endereço: ${elements[5].value}\n                 
                  ${elements[7].value}(${elements[1].value}) nº ${elements[8].value}. ${elements[6].value}\n`
-
+    */
     if (elements[9].value!=""){
         mensagem += `\nApresenta risco de acidente: ${elements[9].value}`
     }
