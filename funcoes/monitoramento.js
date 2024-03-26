b088ee4a797af6cb926da804d0d7cd49ecfc9fd2
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-03-25 22:56:05 -0300

    att

--- funcoes/monitoramento.js
+++ funcoes/monitoramento.js
@@ -87,25 +87,33 @@ function msg_monitorador(msg){
 
 }
 
-Toastify({
-    text: "This is a toast",
-    duration: 3000,
-    destination: "https://github.com/apvarun/toastify-js",
-    newWindow: true,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "left", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-    onClick: function(){} // Callback after click
-  }).showToast();
+// Toastify({
+//     text: "This is a toast",
+//     duration: 3000,
+//     destination: "https://github.com/apvarun/toastify-js",
+//     newWindow: true,
+//     close: true,
+//     gravity: "top", // `top` or `bottom`
+//     position: "left", // `left`, `center` or `right`
+//     stopOnFocus: true, // Prevents dismissing of toast on hover
+//     style: {
+//       background: "linear-gradient(to right, #00b09b, #96c93d)",
+//     },
+//     onClick: function(){} // Callback after click
+//   }).showToast();
 
+
+Toastify({
+
+    text: "🐳 Monitorador",
+    
+    duration: 3000
+    
+    }).showToast();
 
   Toastify({
 
-    text: "This is a toast",
+    text: "🐳 Monitorador\nBem vindo!",
     
     duration: 3000
     
