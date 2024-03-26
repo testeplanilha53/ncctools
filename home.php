ae694014382a9338681449ba3f79f8b4b3d61dcf
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-03-25 22:37:31 -0300

    att

--- home.php
+++ home.php
@@ -328,6 +328,22 @@
     </div>
   </div>
 </div>
+<!-- Flexbox container for aligning the toasts -->
+<div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center w-100">
+
+  <!-- Then put toasts within -->
+  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="..." class="rounded me-2" alt="...">
+      <strong class="me-auto">🐳 Monitorador 2</strong>
+      <small>11 mins ago</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+        Olá, bem vindo!
+    </div>
+  </div>
+</div>
 
 	
 
