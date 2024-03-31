a2eeca4dec0e1899f3d8e75257fec089270942a5
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-03-31 19:18:27 -0300

    att

--- home.php
+++ home.php
@@ -144,7 +144,7 @@
                     <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
                 </svg> 
                 <span data-toggle="tooltip" data-placement="bottom" title="Use seus modelos de preenchimento totalmente personalizáveis.">
-                    &nbsp Modelos de preenchimento 
+                    &nbsp Modelos 
                 </span>
         </button>
         
