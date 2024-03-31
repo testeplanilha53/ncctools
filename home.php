4b724ca37a02955a0d7c4028040c62b70e61cecb
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-03-31 19:34:54 -0300

    att

--- home.php
+++ home.php
@@ -176,7 +176,7 @@
         	  
     
     <label class="navbar-item d-flex justify-content-end _header_label">
-      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 18 18" onclick="menu()">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 18 18" onclick="menu()" id="menu_lateral">
         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
       </svg>
     </label>
