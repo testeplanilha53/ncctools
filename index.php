d36bd2ac3a09d63b4c55cbd2a6a1d923b1cd94af
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-04-18 03:13:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -281,6 +281,158 @@
         }
 
 
+                        .ag-format-container {
+                  width: 900px;
+                  margin: 0 auto;
+                  //background-color: red;
+                }
+
+
+
+                .ag-courses_box {
+                  display: -webkit-box;
+                  display: -ms-flexbox;
+                  display: flex;
+                  -webkit-box-align: center;
+                  -ms-flex-align: center;
+                  align-items: center;
+                  -ms-flex-wrap: wrap;
+                  flex-wrap: wrap;
+                  flex-direction: column;
+                
+                  padding: 0;
+
+                   position: absolute;
+                  top: 20;
+                }
+                .ag-courses_item {
+                  -ms-flex-preferred-size: calc(33.33333% - 30px);
+                  flex-basis: calc(50.33333% - 30px);
+                
+                  margin: 0 15px 30px;
+                
+                  overflow: hidden;
+                
+                  border-radius: 30px;
+                }
+                .ag-courses-item_link {
+                  display: block;
+                  padding: 30px 20px;
+                  background-color: #121212;
+                
+                  overflow: hidden;
+                
+                  position: relative;
+                }
+                .ag-courses-item_link:hover,
+                .ag-courses-item_link:hover .ag-courses-item_date {
+                  text-decoration: none;
+                  color: #FFF;
+                }
+                .ag-courses-item_link:hover .ag-courses-item_bg {
+                  -webkit-transform: scale(10);
+                  -ms-transform: scale(10);
+                  transform: scale(10);
+                }
+                .ag-courses-item_title {
+                  min-height: 5px;
+                  margin: 0 0 25px;
+                
+                  overflow: hidden;
+                
+                  font-weight: bold;
+                  font-size: 30px;
+                  color: #FFF;
+                
+                  z-index: 2;
+                  position: relative;
+                }
+                .ag-courses-item_date-box {
+                  font-size: 18px;
+                  color: #FFF;
+                
+                  z-index: 2;
+                  position: relative;
+                }
+                .ag-courses-item_date {
+                  font-weight: bold;
+                  color: #f9b234;
+                
+                  -webkit-transition: color .5s ease;
+                  -o-transition: color .5s ease;
+                  transition: color .5s ease
+                }
+                .ag-courses-item_bg {
+                  height: 188px;
+                  width: 188px;
+                  background-color: #f9b234;
+                
+                  z-index: 1;
+                  position: absolute;
+                  top: -75px;
+                  right: -75px;
+                
+                  border-radius: 50%;
+                
+                  -webkit-transition: all .5s ease;
+                  -o-transition: all .5s ease;
+                  transition: all .5s ease;
+                }
+                .ag-courses_item:nth-child(2n) .ag-courses-item_bg {
+                  background-color: #3ecd5e;
+                }
+                .ag-courses_item:nth-child(3n) .ag-courses-item_bg {
+                  background-color: #e44002;
+                }
+                .ag-courses_item:nth-child(4n) .ag-courses-item_bg {
+                  background-color: #952aff;
+                }
+                .ag-courses_item:nth-child(5n) .ag-courses-item_bg {
+                  background-color: #cd3e94;
+                }
+                .ag-courses_item:nth-child(6n) .ag-courses-item_bg {
+                  background-color: #4c49ea;
+                }
+                
+                
+                
+                @media only screen and (max-width: 979px) {
+                  .ag-courses_item {
+                    -ms-flex-preferred-size: calc(50% - 30px);
+                    flex-basis: calc(50% - 30px);
+                  }
+                  .ag-courses-item_title {
+                    font-size: 24px;
+                  }
+                }
+                
+                @media only screen and (max-width: 767px) {
+                  .ag-format-container {
+                    width: 96%;
+                  }
+                
+                }
+                @media only screen and (max-width: 639px) {
+                  .ag-courses_item {
+                    -ms-flex-preferred-size: 100%;
+                    flex-basis: 100%;
+                  }
+                  .ag-courses-item_title {
+                    min-height: 72px;
+                    line-height: 1;
+                
+                    font-size: 24px;
+                  }
+                  .ag-courses-item_link {
+                    padding: 22px 40px;
+                  }
+                  .ag-courses-item_date-box {
+                    font-size: 16px;
+                  }
+                }
+
+
+
     </style>
 </head>
 
@@ -479,6 +631,58 @@
 
 <?php   
 
+                   
+
+
+                    if (isset($_GET['p'])) {
+                        $abcdef = base64_decode($_GET['p']);
+                        //echo('<h1>' . $abcdef . '</h1>');
+
+
+                        $dataAtual = new DateTime();
+                        //echo "Data atual: " . $dataAtual->format("Y-m-d");
+
+                        if($dataAtual->format("Y-m-d") == $abcdef){
+                            echo('<div class="ag-format-container">
+                                  <div class="ag-courses_box">
+                                    <div class="ag-courses_item">
+                                      <a href="http://vitrine.rf.gd" class="ag-courses-item_link" >
+                                        <div class="ag-courses-item_bg"></div>
+                                
+                                        <div class="ag-courses-item_title">
+                                          Acesse a vitrine de projetos para ficar atento aos novos apps
+                                        </div>
+                                  
+                                        <span class="ag-courses-item_date">
+                                            18.04.2024
+                                          </span>
+                                        
+                                      </a>
+                                    </div>
+                                  </div>
+                                </div>');
+                        }
+                        
+                    } else {
+                        echo('<div class="ag-format-container">
+                                  <div class="ag-courses_box">
+                                    <div class="ag-courses_item">
+                                      <a href="http://vitrine.rf.gd" class="ag-courses-item_link" >
+                                        <div class="ag-courses-item_bg"></div>
+                                
+                                        <div class="ag-courses-item_title">
+                                          Acesse a vitrine de projetos para ficar atento aos novos apps
+                                        </div>
+                                  
+                                        <span class="ag-courses-item_date">
+                                            18.04.2024
+                                          </span>
+                                        
+                                      </a>
+                                    </div>
+                                  </div>
+                                </div>');
+                    }
     
                     if( isset($_GET['action']) && $_GET['action'] == "error" ){
                          echo('<script>window.error.showModal(); </script>');
