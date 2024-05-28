c60fc88adea64065c5e0235b7ee53b47b9ba7b93
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-05-28 20:06:13 -0300

    a

--- teste/_components/header.js
+++ teste/_components/header.js
@@ -1,69 +1,59 @@
 function  header(){
     return(
-    `
-<!-- Area de apps -->
-			<!--
-			<div class="nav-scroller bg-body rounded shadow-sm border bg-dark-subtle">
-				<nav class="nav nav-underline" aria-label="Secondary navigation">
-					<a class="nav-link" href="https://ncctools.epizy.com/teste/#">
-						Notificações
-						<span class="badge bg-light text-dark rounded-pill align-text-bottom">27</span>
-					</a>
-					<a class="nav-link" href="https://ncctools.epizy.com/teste/#">Documentação</a>
-					<a class="nav-link active" data-bs-toggle="modal" data-bs-target="#textModelsModal"
-						href="https://ncctools.epizy.com/teste/#">Modelos</a>
-					<a class="nav-link" data-bs-toggle="modal" data-bs-target="#nocModal"
-						href="https://ncctools.epizy.com/teste/#">App NOC</a>
-					<a class="nav-link" data-bs-toggle="modal" data-bs-target="#servicesModal"
-						href="https://ncctools.epizy.com/teste/#">App Serviços</a>
-					<a class="nav-link" data-bs-toggle="modal" data-bs-target="#lgpdModal"
-						href="https://ncctools.epizy.com/teste/#">App LGPD</a>
-
-				</nav>
+		`
+		<!-- As a link -->
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Tools 2.0</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+					aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" data-bs-toggle="modal" data-bs-target="#notificacoesModal" href="#">
+								Notificações
+								<span class="badge bg-light text-dark rounded-pill align-text-bottom">27</span>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Documentação</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link active" data-bs-toggle="modal" data-bs-target="#textModelsModal"
+								href="#">Modelos</a>
+						</li>
+	
+						<li>
+							<a class="nav-link" data-bs-toggle="modal" data-bs-target="#nocModal" href="#">App NOC</a>
+						</li>
+						<li>
+							<a class="nav-link" data-bs-toggle="modal" data-bs-target="#servicesModal" href="#">App
+								Serviços</a>
+						</li>
+						<li>
+							<a class="nav-link" data-bs-toggle="modal" data-bs-target="#lgpdModal" href="#">App LGPD</a>
+						</li>
+	
+	
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+								aria-expanded="false">
+								Usuário
+							</a>
+							<ul class="dropdown-menu">
+								<li><a class="dropdown-item" href="#">Notificações</a></li>
+								<li><a class="dropdown-item" href="#">Configurações</a></li>
+								<hr>
+								<li><a class="dropdown-item" href="#">Sair</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
 			</div>
-			-->
-
-			<!-- Aba de clientes  -->
-			<ul class="nav nav-tabs bg-body rounded shadow-sm border bg-dark-subtle">
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-						aria-expanded="false">Ana</a>
-					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="#">Alterar nome</a></li>
-						<li><a class="dropdown-item" href="#">Alterar tag de cor</a></li>
-						<li><a class="dropdown-item" href="#">Deixar pendente</a></li>
-						<li>
-							<hr class="dropdown-divider">
-						</li>
-						<li><a class="dropdown-item" href="#" onclick="AlterClient()">Fechar</a></li>
-					</ul>
-				</li>
-
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle nav-link active" data-bs-toggle="dropdown" href="#"
-						role="button" aria-expanded="false">Jose</a>
-					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="#">Alterar nome</a></li>
-						<li><a class="dropdown-item" href="#">Alterar tag de cor</a></li>
-						<li><a class="dropdown-item" href="#">Deixar pendente</a></li>
-						<li>
-							<hr class="dropdown-divider">
-						</li>
-						<li><a class="dropdown-item" href="#">Fechar</a></li>
-					</ul>
-				</li>
-
-				<li class="nav-item">
-					<a class="nav-link" href="#">Jessica</a>
-				</li>
-
-				<li class="nav-item" onclick="AddClient()">
-					<a class="nav-link disabled" aria-disabled="true">Adicionar</a>
-				</li>
-
-			</ul><!-- Nav tab clientes -->
-
-			`
+		</nav>
+		`
 )}
 
 
--- teste/_components/modal.js
+++ teste/_components/modal.js
@@ -1,59 +1,6 @@
 function  modal(){
     return(
     `
-	<!-- As a link -->
-	<nav class="navbar navbar-expand-lg bg-body-tertiary">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Tools 2.0</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" data-bs-toggle="modal" data-bs-target="#notificacoesModal" href="#">
-							Notificações
-							<span class="badge bg-light text-dark rounded-pill align-text-bottom">27</span>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Documentação</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" data-bs-toggle="modal" data-bs-target="#textModelsModal"
-							href="#">Modelos</a>
-					</li>
-
-					<li>
-						<a class="nav-link" data-bs-toggle="modal" data-bs-target="#nocModal" href="#">App NOC</a>
-					</li>
-					<li>
-						<a class="nav-link" data-bs-toggle="modal" data-bs-target="#servicesModal" href="#">App
-							Serviços</a>
-					</li>
-					<li>
-						<a class="nav-link" data-bs-toggle="modal" data-bs-target="#lgpdModal" href="#">App LGPD</a>
-					</li>
-
-
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-							aria-expanded="false">
-							Usuário
-						</a>
-						<ul class="dropdown-menu">
-							<li><a class="dropdown-item" href="#">Notificações</a></li>
-							<li><a class="dropdown-item" href="#">Configurações</a></li>
-							<hr>
-							<li><a class="dropdown-item" href="#">Sair</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-
 	<div class="modal fade" id="notificacoesModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 		<div class="modal-dialog modal-dialog-scrollable">
 			<div class="modal-content">
