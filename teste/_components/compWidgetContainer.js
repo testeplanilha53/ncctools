088d9d5d709b1be26be959b703d11658313d90a1
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-05-28 22:05:58 -0300

    a

--- teste/_components/compWidgetContainer.js
+++ teste/_components/compWidgetContainer.js
@@ -1,10 +1,10 @@
-function WidgetContainer(){
+function WidgetContainer(content = "Content"){
     return(`
     <div class="my-3 p-3 bg-body rounded shadow-sm border bg-dark-subtle">
         <label for="exampleFormControlTextarea1" class="form-label">Agendamento</label>
         </div><!--end input container-->
 
-        Teste
+        ${content}
 
     </div> <!--End container shadow-sm-->
     
