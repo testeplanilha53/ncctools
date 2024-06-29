57c6baf53c797bd87d3f114c6c177a3ed03aaa47
testeplanilha53 <122693822+testeplanilha53@users.noreply.github.com>
2024-06-28 23:10:22 -0300

    att

--- funcoes/modal_pontoADC.js
+++ funcoes/modal_pontoADC.js
@@ -1,7 +1,4 @@
 
-
-
-
 function calcularPontoAdicional(){
 
     var plano_mega = window.document.getElementById("plano_mega").value
@@ -9,21 +6,31 @@ function calcularPontoAdicional(){
     // var qtd_parcelas = window.document.getElementById("qtd_parcelas").value        
     var pontos_adicionais = window.document.getElementById("pontos_adicionais").value
     var valorPlano = window.document.getElementById("valorPlano_adc").value    
+    // valorPlano = valorPlano.toFixed(2)
+    valorPlano_str = valorPlano.toString()
+    valorPlano_str = substituirPontoPorVirgula(valorPlano_str)
+    
     
     // var vInsta = valorInstalação(pontos_adicionais)    
     // console.log(vInsta)
 
     var vMensalidade = valorMensalidade(pontos_adicionais)    
     console.log(vMensalidade)
+
+    vMensalidade = vMensalidade.toFixed(2)
+    vMensalidade_str = vMensalidade.toString()
+    vMensalidade_str = substituirPontoPorVirgula(vMensalidade_str)
+
     // console.log(substituirPontoPorVirgula(vMensalidade))
 
     // console.log(typeof(valorPlano))
     // console.log(typeof(vMensalidade))
     
     var Total = (valorPlano*1) + (vMensalidade*1)    
-    console.log(Total)
+    // console.log(Total)
     Total = Total.toFixed(2)
-    console.log(substituirPontoPorVirgula(Total))
+    Total_str = Total.toString()
+    Total_str = substituirPontoPorVirgula(Total_str)
     
     
     // var vParcela = vInsta/qtd_parcelas
@@ -32,8 +39,8 @@ function calcularPontoAdicional(){
     
     // antigo
     // var script = `Cliente solicitou a instalação de ${pontos_adicionais} ${pluralPontos(pontos_adicionais)} [wi-fi]. Ficou ciente do prazo de até 7 dias para a ocorrência do serviço.<hr><b>Valor da instalação:</b> R$${vInsta} (em ${qtd_parcelas}x).\n<b>Valor da mensalidade de ${pontos_adicionais} ${pluralPontos(pontos_adicionais)}:</b> R$${vMensalidade}.<hr>Cliente possui um plano de ${plano_mega} ${plan_tipo}, nesse caso o valor da mensalidade ficará: R$${valorPlano} + R$${vMensalidade} = R$${Total}, além de R$${vParcela} da instalação durante ${qtd_parcelas} ${verMes(qtd_parcelas)}.`
-    var script = `Cliente solicitou a instalação de ${pontos_adicionais} ${pluralPontos(pontos_adicionais)} [wi-fi]. Ficou ciente do prazo de até 7 dias para a ocorrência do serviço.\n<hr><b>Valor da mensalidade de ${pontos_adicionais} ${pluralPontos(pontos_adicionais)}:</b> R$${vMensalidade}.<hr>Cliente possui um plano de ${plano_mega} ${plan_tipo}, nesse caso o valor da mensalidade ficará: R$${valorPlano} + R$${vMensalidade} = R$${Total}.`    
-    console.log(script)
+    var script = `Cliente solicitou a instalação de ${pontos_adicionais} ${pluralPontos(pontos_adicionais)} [wi-fi]. Ficou ciente do prazo de até 7 dias para a ocorrência do serviço.\n<hr><b>Valor da mensalidade de ${pontos_adicionais} ${pluralPontos(pontos_adicionais)}:</b> R$${vMensalidade}.<hr>Cliente possui um plano de ${plano_mega} ${plan_tipo}, nesse caso o valor da mensalidade ficará: R$${valorPlano_str} + R$${vMensalidade_str} = R$${Total_str}.`    
+    // console.log(script)
     
 
     var descricao = window.document.getElementById("descricao")  
